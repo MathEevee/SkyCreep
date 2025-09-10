d398a8115f5f650a273e84e807d248fc9059b4df
Mathilde De Oliveira <matde-ol@1H1.42angouleme.fr>
2025-09-10 03:14:27 +0200

    supp code

--- src/item.cpp
+++ src/item.cpp
@@ -1,36 +1,5 @@
 #include "item.hpp"
 
-std::string select_pars_item(std::string old_name, std::map<std::string, std::string> dictionary)
-{
-	return (dictionary[old_name]);
-}
-
-
-std::string change_name(std::string old_name, std::map<std::string, std::string> dictionary)
-{
-	std::string new_name = dictionary[old_name];
-	if (new_name != old_name)
-		return (new_name);
-
-	if (old_name.find(' ') == std::string::npos)
-		return (old_name);
-
-	new_name = "";
-
-	while (old_name.find(' ') != std::string::npos)
-	{
-		new_name = old_name.substr(0, old_name.find(' '));
-		old_name = old_name.substr(old_name.find(' ') + 1);
-		if (old_name[0])
-		{
-			if (old_name[0] >= 'a' && old_name[0] <= 'z')
-				old_name[0] = old_name[0] - 32;
-		}
-		new_name = new_name + old_name;
-	}
-	return (new_name);
-}
-
 Item::Item()
 {}
 
@@ -38,7 +7,7 @@ Item::Item(std::string name, std::map<std::string, std::string> dictionary)
 {
 	std::string new_name;
 
-	new_name = change_name(name, dictionary);
+	new_name = dictionary[name];
 
 	this->_name_item = new_name;
 }
